3f234fa8c2562127fa7d74e9e5123ce89d972f54
arindas <arindas@users.noreply.github.com>
2024-08-10 06:56:16 +0000

    deploy: 27922e29824d934e53cab1f75daad58c16e449dd

--- docs/trait.impl/core/default/trait.Default.js
+++ docs/trait.impl/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"laminarmq_io":[["impl&lt;F&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"laminarmq_io/object_storage/aws_s3/struct.GetObjectOutputFuture.html\" title=\"struct laminarmq_io::object_storage::aws_s3::GetObjectOutputFuture\">GetObjectOutputFuture</a>&lt;F&gt;"]]
+"laminarmq_io":[["impl&lt;F&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"laminarmq_io/object_storage/aws_s3/struct.GetObjectOutputFuture.html\" title=\"struct laminarmq_io::object_storage::aws_s3::GetObjectOutputFuture\">GetObjectOutputFuture</a>&lt;F&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
