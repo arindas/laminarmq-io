d97a26092de4e0f34866cf8c1c69bb58a27185d6
Arindam Das <dasarindam.mails@gmail.com>
2024-08-15 12:36:19 +0530

    feat: adds AsyncFlush impl for BufferredAppender

--- src/buffered_io.rs
+++ src/buffered_io.rs
@@ -1,11 +1,13 @@
 use std::{marker::PhantomData, ops::Deref};
 
 use bytes::Bytes;
+use num::ToPrimitive;
 
 use crate::{
     anchored_buffer::{AnchoredBuffer, BufferError},
     io_types::{
-        AsyncRead, ByteLender, FallibleEntity, IntegerConversionError, ReadBytes, SizedEntity,
+        AppendLocation, AsyncAppend, AsyncFlush, AsyncRead, ByteLender, FallibleEntity,
+        IntegerConversionError, ReadBytes, SizedEntity, UnwrittenError,
     },
 };
 
@@ -42,8 +44,8 @@ where
 #[allow(unused)]
 pub struct BufferedAppender<R, P, S> {
     inner: R,
-
     buffer: AnchoredBuffer<P>,
+    flush_state: FlushState,
     size: S,
 }
 
@@ -54,6 +56,8 @@ pub enum BufferedAppenderError<E> {
     CloseError(E),
 
     AppendBufferError(BufferError),
+
+    InvalidWriteSize,
 
     ReadBeyondWrittenArea,
     ReadGapEncountered,
@@ -118,6 +122,71 @@ where
                 .map(|x| x.map(BufferedReadByteBuf::Read))
                 .map_err(Self::Error::ReadError),
             _ => Err(Self::Error::ReadGapEncountered),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum FlushState {
+    Incomplete { retry_flush_buffer_offset: usize },
+    Clean,
+}
+
+impl<R> AsyncFlush for BufferedAppender<R, R::Position, R::Size>
+where
+    R: SizedEntity + AsyncFlush + AsyncAppend,
+{
+    async fn flush(&mut self) -> Result<(), Self::Error> {
+        let flush_buffer_offset = match self.flush_state {
+            FlushState::Incomplete {
+                retry_flush_buffer_offset,
+            } => retry_flush_buffer_offset,
+            FlushState::Clean => 0,
+        };
+
+        let bytes = self
+            .buffer
+            .get_read_slice(flush_buffer_offset..)
+            .map_err(Self::Error::AppendBufferError)?;
+
+        let bytes_len = bytes.len();
+
+        let inner_append_result = self.inner.append(bytes).await;
+
+        match inner_append_result {
+            Ok(AppendLocation {
+                write_position,
+                write_len,
+            }) if write_len
+                .to_usize()
+                .ok_or(Self::Error::IntegerConversionError)?
+                == bytes_len =>
+            {
+                self.flush_state = FlushState::Clean;
+                self.buffer.re_anchor(write_position + write_len.into());
+
+                self.inner.flush().await.map_err(Self::Error::AppendError)?;
+
+                Ok(())
+            }
+
+            Ok(AppendLocation {
+                write_position,
+                write_len,
+            }) => {
+                self.flush_state = FlushState::Incomplete {
+                    retry_flush_buffer_offset: self
+                        .buffer
+                        .offset(write_position + write_len.into())
+                        .ok_or(Self::Error::InvalidWriteSize)?,
+                };
+
+                self.inner.flush().await.map_err(Self::Error::AppendError)?;
+
+                Err(Self::Error::FlushIncomplete)
+            }
+
+            Err(UnwrittenError { unwritten: _, err }) => Err(Self::Error::AppendError(err)),
         }
     }
 }
